608b94403a7a978ada00acd4473204c090dbd8ba
lgq <138521257@qq.com>
2022-09-18 23:19:56 +0800

    小程序

--- coolcar/wx/miniprogram/pages/index/index.ts
+++ coolcar/wx/miniprogram/pages/index/index.ts
@@ -1,15 +1,35 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
+import { IAppOption } from "../../appoption"
 
 Page({
   data: {
-    motto: 'Hello World from typescript',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  avatarURL: '',
+  setting: {
+    skew: 0,
+    rotate: 0,
+    showLocation: true,
+    showScale: true,
+    subKey: '',
+    layerStyle: -1,
+    enableZoom: true,
+    enableScroll: true,
+    enableRotate: false,
+    showCompass: false,
+    enable3D: false,
+    enableOverlooking: false,
+    enableSatellite: false,
+    enableTraffic: false,
+  },
+  location: {
+    latitude: 30,
+    longitude: 120,
+  },
+  scale: 16,
+  },
+  async onLoad() {
+    const userInfo = await getApp<IAppOption>().globalData.userInfo
+    this.setData({
+      avatarURL: userInfo.avatarUrl,
+    })
   },
   // 事件处理函数
   bindViewTap() {
@@ -17,33 +37,43 @@ Page({
       url: '../logs/logs',
     })
   },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
+  onMyLocationTap() {
+    // wx.getSetting({
+    //   success(res){
+    //     if(res.authSetting['scope.userLocation']===false){
+    //       wx.openSetting({
+    //         success(res){
+    //         res.authSetting ={
+    //           "scope.userLocation": true
+    //         }
+    //       }
+    //       })
+    //     }else{
+    //       wx.authorize({
+    //         scope: 'scope.userLocation',
+    //         success(){
+
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+    wx.getLocation({
+      type: 'gcj02',
+      success: res => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude,
+          },
+        })
+      }, 
+      fail: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '请前往设置页授权',
         })
       }
     })
   },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
